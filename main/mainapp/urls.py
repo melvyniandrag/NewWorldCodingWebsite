8eae6c9bcace24692fd883ba07b2fd2fb47261cd
Melvyn Drag Laptop <melvyniandrag@gmail.com>
2024-12-31 03:53:21 -0500

    added more stuff

--- main/mainapp/urls.py
+++ main/mainapp/urls.py
@@ -5,7 +5,14 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("comingsoon", views.coming_soon_view, name="coming_soon_view"),
+    path("teaching", views.teaching_view, name="teaching_view"),
+    path("horizon", views.horizon_view, name="horizon_view"),
     path("workshops/webdevelopment", views.workshop_webdevelopment_detail_view, name="workshop_webdevelopment_detail_view"),
+    path("workshops/textencodings", views.workshop_textencoding_detail_view, name="workshop_textencoding_detail_view"),
+    path("workshops/arduinorobot", views.workshop_arduinorobot_detail_view, name="workshop_arduinorobot_detail_view"),
+    path("workshops/gameboy", views.workshop_gameboy_detail_view, name="workshop_gameboy_detail_view"),
+    path("workshops/introtokotlin", views.workshop_introtokotlin_detail_view, name="workshop_introtokotlin_detail_view"),
+    path("workshops/introtolinux", views.workshop_introtolinux_detail_view, name="workshop_introtolinux_detail_view"),
     path("create", views.create_view, name="create_view"),
     path("list", views.list_view, name="list_view"),
 ]
--- main/mainapp/views.py
+++ main/mainapp/views.py
@@ -13,10 +13,44 @@ def coming_soon_view(request):
     context ={}
     return render(request, "mainapp/coming_soon_view.html", context) 
 
+def teaching_view(request):
+    context ={}
+    return render(request, "mainapp/teaching_view.html", context) 
+
+
+def horizon_view(request):
+    context ={}
+    return render(request, "mainapp/horizon_view.html", context) 
+
 
 def workshop_webdevelopment_detail_view(request):
     context ={}
     return render(request, "mainapp/workshop_webdevelopment_detail_view.html", context) 
+
+
+def workshop_textencoding_detail_view(request):
+    context ={}
+    return render(request, "mainapp/coming_soon_view.html", context) 
+
+
+def workshop_arduinorobot_detail_view(request):
+    context ={}
+    return render(request, "mainapp/coming_soon_view.html", context) 
+
+
+def workshop_gameboy_detail_view(request):
+    context ={}
+    return render(request, "mainapp/coming_soon_view.html", context) 
+
+
+def workshop_introtokotlin_detail_view(request):
+    context ={}
+    return render(request, "mainapp/coming_soon_view.html", context) 
+    
+
+def workshop_introtolinux_detail_view(request):
+    context ={}
+    return render(request, "mainapp/coming_soon_view.html", context) 
     
 
 def list_view(request):
